b257f08dbc48378af90657a8ef31c62b974d93f0
Sathiyaseelan Madheswaran <62843219+sathiya1605@users.noreply.github.com>
2020-12-12 16:14:44 +0530

    Update gsheetapi.py

--- src/api/gsheetapi.py
+++ src/api/gsheetapi.py
@@ -3,5 +3,5 @@ import gspread
 # -----------------Google Sheets API Credentials--------------------------#
 
 sheets_acc = gspread.service_account(filename='filename.json')  # filename='client_secret.json'
-sheet_id = sheets_acc.open_by_key('1x1sjE6k1bZRS77fQ-8B-LzYdssf_UaGeRyKMrFZYceY')
-worksheet = sheet_id.worksheet('Sheet')
+sheet_id = sheets_acc.open_by_key('1x1sjE6k1bZRS77fQ-8B-LzYdssf_UaGeRyKMrFZYceY') # sheet id
+worksheet = sheet_id.worksheet('Sheet') # worksheet name
